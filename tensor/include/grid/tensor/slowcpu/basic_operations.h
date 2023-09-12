62bcb9fc1f9a5a18697a052e76496bc57c948c03
Chris Zankel <chris@zankel.net>
2023-09-11 23:48:14 -0700

    slowcpu: refactor TensorAdd functor
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/slowcpu/basic_operations.h
+++ tensor/include/grid/tensor/slowcpu/basic_operations.h
@@ -108,93 +108,51 @@ struct TensorAdd<TensorSlowCpu, _T, _Rank, _Tensor1, _Tensor2> : TensorBaseOp //
   TensorAdd& operator=(const TensorAdd& other) = delete;
   TensorAdd& operator=(TensorAdd&& other) = delete;
 
+  // TODO: move conditional up the call chain (create addslow and addfast call-chains)
+  inline void add(char* dest, const char* src1, const char* src2,
+                  std::span<const size_t,  1> dims,
+                  std::span<const ssize_t, 1>,
+                  std::span<const ssize_t, 1> strides1,
+                  std::span<const ssize_t, 1> strides2) const
+  {
+    value_type* datadest = reinterpret_cast<value_type*>(dest);
+    for (size_t i = 0; i < dims[0]; i++, src1 += strides1[0], src2 += strides2[0])
+      datadest[i] = *reinterpret_cast<const value_type*>(src1) + *reinterpret_cast<const value_type*>(src2);
+  }
+
+  template <size_t _N> inline
+  void add(char* dest, const char* src1, const char* src2,
+           std::span<const size_t,  _N> dims,
+           std::span<const ssize_t, _N> strides0,
+           std::span<const ssize_t, _N> strides1,
+           std::span<const ssize_t, _N> strides2) const
+  {
+    static_assert(_N != std::dynamic_extent, "dynamic_extent not allowed");
+    for (size_t i = 0; i < dims[0]; i++, dest += strides0[0], src1 += strides1[0], src2 += strides2[0])
+      add(dest, src1, src2,
+          std::span<const size_t,  _N - 1>(dims.begin() + 1, _N - 1),
+          std::span<const ssize_t, _N - 1>(strides0.begin() + 1, _N - 1),
+          std::span<const ssize_t, _N - 1>(strides1.begin() + 1, _N - 1),
+          std::span<const ssize_t, _N - 1>(strides2.begin() + 1, _N - 1));
+  }
+
   // Functor
-
-  // Rank1 + Rank1
-  template <TensorRank<1> = _Tensor1, TensorRank<1> = _Tensor2>
+  // FIXME: must be same rank? static_assert?
   auto operator()() const
   {
-    size_t dim_m = tensor1_.Dims()[0];
+    auto& dims = tensor1_.Dims();
+    auto result = TensorSlowCpu(dims, Uninitialized<value_type>{});
 
-    // FIXME: stride
-    const value_type* data1 = reinterpret_cast<const value_type*>(tensor1_.Data());
-    const value_type* data2 = reinterpret_cast<const value_type*>(tensor2_.Data());
-
-    // FIXME: this would be a local variable???
-    auto res = TensorSlowCpu(dim_m, Uninitialized<value_type>{});
-    value_type* sum = reinterpret_cast<value_type*>(res.Data());
-
-    for (size_t i = 0; i < dim_m; i++)
-      sum[i] = data1[i] + data2[i];
-    //auto res = TensorSlowCpu(0, Uninitialized<value_type>{});
-    return res;
+    add(reinterpret_cast<char*>(result.Data()),
+        reinterpret_cast<const char*>(tensor1_.Data()),
+        reinterpret_cast<const char*>(tensor2_.Data()),
+        std::span(dims),
+        std::span(result.Strides()),
+        std::span(tensor1_.Strides()),
+        std::span(tensor2_.Strides()));
+    return result;
   }
 
-  // Rank2 + Rank2
-  template <TensorRank<2> = _Tensor1, TensorRank<2> = _Tensor2>
-  auto operator()() const
-  {
-    size_t dim_m = tensor1_.Dims()[0];
-    size_t dim_n = tensor1_.Dims()[1];
-
-    const value_type* data1 = reinterpret_cast<const value_type*>(tensor1_.Data());
-    const value_type* data2 = reinterpret_cast<const value_type*>(tensor2_.Data());
-
-    auto res = TensorSlowCpu(dim_m, dim_n, Uninitialized<value_type>{});
-    value_type* sum = reinterpret_cast<value_type*>(res.Data());
-
-    // FIXME: handle stride
-    for (size_t i = 0; i < dim_m; i++)
-      for (size_t j = 0; j < dim_n; j++)
-        sum[i * dim_n + j] = data1[i * dim_n + j] + data2[i * dim_n + j];
-#if 0
-    auto& tensor1 = std::get<0>(tensors_);
-    size_t dim_m = tensor1.Dim(0);
-    size_t dim_n = tensor1.Dim(1);
-    auto res = TensorSlowCpu(dim_m, dim_n, Uninitialized<value_type>{});
-#endif
-    return res;
-  }
-#if 0
-  // FIXME: stride
-  // Rank3 + Rank3
-  template <TensorRank<3> = _Tensor1, TensorRank<3> = _Tensor2>
-  auto operator()() const
-  {
-    size_t dim_m = tensor1_.Dim(0);
-    size_t dim_n = tensor1_.Dim(1);
-    size_t dim_c = tensor1_.Dim(2);
-
-    size_t stride1_m = tensor1_.Stride(0);
-    size_t stride1_n = tensor1_.Stride(1);
-    size_t stride1_c = tensor1_.Stride(2);
-    size_t stride2_m = tensor2_.Stride(0);
-    size_t stride2_n = tensor2_.Stride(1);
-    size_t stride2_c = tensor2_.Stride(2);
-
-    const value_type* data1 = tensor1_.Data();
-    const value_type* data2 = tensor2_.Data();
-
-    auto res = TensorSlowCpu(dim_m, dim_n, dim_c, Uninitialized<value_type>{});
-    value_type* sum = res.Data();
-
-    // FAST
-    if (stride1_c == 1 && stride2_c == 1)
-      for (size_t i = 0; i < dim_m; i++)
-        for (size_t j = 0; j < dim_n; j++)
-          for (size_t k = 0; j < dim_c; j++)
-            sum[(i * dim_n + j) * dim_c + k] =
-              data1[(i * dim_n + j) * dim_c + k] + data2[(i * dim_n + j) * dim_c + k];
-    else
-      for (size_t i = 0; i < dim_m; i++)
-        for (size_t j = 0; j < dim_n; j++)
-          for (size_t k = 0; j < dim_c; j++)
-            sum[(i * dim_n + j) * dim_c + k] =
-              data1[(i * dim_n + j) * dim_c + k] + data2[(i * dim_n + j) * dim_c + k];
-
-    return res;
-  }
-#endif
   _Tensor1 tensor1_;
   _Tensor2 tensor2_;
 };
