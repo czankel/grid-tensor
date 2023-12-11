365168e297349c218341468e26877ef9ee515bc9
Chris Zankel <chris@zankel.net>
2023-12-10 21:03:18 -0800

    base: move TensorView into tensor_view.h
    No functional change.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/base/tensor.h
+++ tensor/include/grid/tensor/base/tensor.h
@@ -17,12 +17,11 @@
 
 #include "../array.h"
 #include "../tensor_parameters.h"
+#include "tensor_view.h"
 
 #include "copy.h"
 
 namespace grid {
-
-template <PrimitiveTensor, size_t> class TensorView;
 
 /// Tensor provides an non-optimized base implementation of tensors.
 /// Note that the implementation implicitly requires that the buffer and strides are aligned to the value type.
@@ -583,108 +582,6 @@ class Tensor<_Tp, _Rank, NoAllocator>
   pointer                     data_;
 };
 
-
-/// TensorView<Tensor, Rank> implements a view of a tensor.
-///
-/// Note that a view cannot be created from a temporary rval; it will return a tensor.
-template <PrimitiveTensor _Tensor, size_t _Rank>
-class TensorView
-{
- public:
-  using value_type = typename _Tensor::value_type;
-  using pointer = typename _Tensor::pointer;
-  using const_pointer = typename _Tensor::const_pointer;
-  constexpr static size_t rank = _Rank;
-
- private:
-  // pointer_cast provides a (value) const-qualified pointer cast
-  template <typename T, typename S> requires (std::is_const_v<std::remove_pointer_t<S>>)
-  const std::remove_pointer_t<T>* pointer_cast(S pointer)
-  {
-    return reinterpret_cast<const std::remove_pointer_t<T>*>(pointer);
-  }
-  template <typename T, typename S> requires (!std::is_const_v<std::remove_pointer_t<S>>)
-  std::remove_pointer_t<T>* pointer_cast(S pointer)
-  {
-    return reinterpret_cast<std::remove_pointer_t<T>*>(pointer);
-  }
-
- public:
-  // TensorView must be in the same scope and lifetime as the underlying tensor.
-  TensorView() = delete;
-
-  /// Constructor
-  template <size_t _TensorRank>
-  explicit TensorView(_Tensor& tensor, const ssize_t(& axes)[_Rank], const ssize_t(& offsets)[_TensorRank])
-    : size_(0UL)
-  {
-    std::bitset<_TensorRank> handled = false;
-    auto strides = tensor.Strides();
-    auto dimensions    = tensor.Dimensions();
-
-    for (ssize_t i = static_cast<ssize_t>(_Rank) - 1; i >= 0; i--)
-    {
-      if (axes[i] >= 0 && axes[i] < static_cast<ssize_t>(_TensorRank))
-      {
-        if (handled[axes[i]])
-          throw std::runtime_error("axis can only be used once");
-
-        handled.set(axes[i]);
-        dimensions_[i] = dimensions[axes[i]];
-        strides_[i] = strides[axes[i]];
-        size_ += dimensions_[i] * strides_[i];
-      }
-      else if (axes[i] == Broadcast)
-      {
-        dimensions_[i] = 1;
-        strides_[i] = 0;
-      }
-      else
-        throw std::runtime_error("Invalid axis");
-    }
-
-    size_t offset = 0UL;
-    for (size_t i = 0; i < _TensorRank; i++)
-    {
-      if (offsets[i] > static_cast<ssize_t>(tensor.dimensions_[i] * tensor.strides_[i]))
-        throw std::runtime_error("Offset exceeds dimension");
-      offset += offsets[i] * tensor.strides_[i];
-    }
-
-    data_ = reinterpret_cast<pointer>(pointer_cast<char*>(tensor.Data()) + offset);
-  }
-
-
-  /// operator=(Tensor) copies data from the rhs tensor (or view) into the view of the dependent tensor.
-  template <AnyTensor _FromTensor> requires (_FromTensor::rank == _Rank)
-  auto operator=(const _FromTensor& rhs)
-  {
-    copy<value_type, _Rank>(data_, rhs.Data(), dimensions_, strides_, rhs.Strides());
-  }
-
-
-  /// Rank returns the rank of the tensor.
-  constexpr static size_t Rank()                          { return _Rank; }
-
-  /// Dimensions returns the dimensions of the tensor.
-  const std::array<size_t, _Rank>& Dimensions() const     { return dimensions_; }
-
-  /// Strides returns the strides of the tensor.
-  const std::array<ssize_t, _Rank>& Strides() const       { return strides_; }
-
-  /// Size returns the data buffer size.
-  size_t Size() const                                     { return size_; }
-
-  /// Data returns a pointer to the data buffer.
-  pointer Data()                                          { return &data_; }
-  const_pointer Data() const                              { return data_; }
-
- private:
-  std::array<size_t, _Rank>   dimensions_;
-  std::array<ssize_t, _Rank>  strides_;
-  size_t                      size_;
-  pointer                     data_;
-};
 
 //
 // CTAD rules
---
+++ tensor/include/grid/tensor/base/tensor_view.h
@@ -0,0 +1,123 @@
+//
+// Copyright (C) Chris Zankel. All rights reserved.
+// This code is subject to U.S. and other copyright laws and
+// intellectual property protections.
+//
+// The contents of this file are confidential and proprietary to Chris Zankel.
+//
+
+// DO NOT INCLUDE THIS FILE DIRECTLY
+
+#ifndef GRID_TENSOR_BASE_TENSOR_VIEW_H
+#define GRID_TENSOR_BASE_TENSOR_VIEW_H
+
+#include "copy.h"
+
+namespace grid {
+
+/// TensorView<Tensor, Rank> implements a view of a tensor.
+///
+/// Note that a view cannot be created from a temporary rval; it will return a tensor.
+template <PrimitiveTensor _Tensor, size_t _Rank>
+class TensorView
+{
+ public:
+  using value_type = typename _Tensor::value_type;
+  using pointer = typename _Tensor::pointer;
+  using const_pointer = typename _Tensor::const_pointer;
+  constexpr static size_t rank = _Rank;
+
+ private:
+  // pointer_cast provides a (value) const-qualified pointer cast
+  template <typename T, typename S> requires (std::is_const_v<std::remove_pointer_t<S>>)
+  const std::remove_pointer_t<T>* pointer_cast(S pointer)
+  {
+    return reinterpret_cast<const std::remove_pointer_t<T>*>(pointer);
+  }
+  template <typename T, typename S> requires (!std::is_const_v<std::remove_pointer_t<S>>)
+  std::remove_pointer_t<T>* pointer_cast(S pointer)
+  {
+    return reinterpret_cast<std::remove_pointer_t<T>*>(pointer);
+  }
+
+ public:
+  // TensorView must be in the same scope and lifetime as the underlying tensor.
+  TensorView() = delete;
+
+  /// Constructor
+  template <size_t _TensorRank>
+  explicit TensorView(_Tensor& tensor, const ssize_t(& axes)[_Rank], const ssize_t(& offsets)[_TensorRank])
+    : size_(0UL)
+  {
+    std::bitset<_TensorRank> handled = false;
+    auto strides = tensor.Strides();
+    auto dimensions    = tensor.Dimensions();
+
+    for (ssize_t i = static_cast<ssize_t>(_Rank) - 1; i >= 0; i--)
+    {
+      if (axes[i] >= 0 && axes[i] < static_cast<ssize_t>(_TensorRank))
+      {
+        if (handled[axes[i]])
+          throw std::runtime_error("axis can only be used once");
+
+        handled.set(axes[i]);
+        dimensions_[i] = dimensions[axes[i]];
+        strides_[i] = strides[axes[i]];
+        size_ += dimensions_[i] * strides_[i];
+      }
+      else if (axes[i] == Broadcast)
+      {
+        dimensions_[i] = 1;
+        strides_[i] = 0;
+      }
+      else
+        throw std::runtime_error("Invalid axis");
+    }
+
+    size_t offset = 0UL;
+    for (size_t i = 0; i < _TensorRank; i++)
+    {
+      if (offsets[i] > static_cast<ssize_t>(tensor.dimensions_[i] * tensor.strides_[i]))
+        throw std::runtime_error("Offset exceeds dimension");
+      offset += offsets[i] * tensor.strides_[i];
+    }
+
+    data_ = reinterpret_cast<pointer>(pointer_cast<char*>(tensor.Data()) + offset);
+  }
+
+
+  /// operator=(Tensor) copies data from the rhs tensor (or view) into the view of the dependent tensor.
+  template <AnyTensor _FromTensor> requires (_FromTensor::rank == _Rank)
+  auto operator=(const _FromTensor& rhs)
+  {
+    copy<value_type, _Rank>(data_, rhs.Data(), dimensions_, strides_, rhs.Strides());
+  }
+
+
+  /// Rank returns the rank of the tensor.
+  constexpr static size_t Rank()                          { return _Rank; }
+
+  /// Dimensions returns the dimensions of the tensor.
+  const std::array<size_t, _Rank>& Dimensions() const     { return dimensions_; }
+
+  /// Strides returns the strides of the tensor.
+  const std::array<ssize_t, _Rank>& Strides() const       { return strides_; }
+
+  /// Size returns the data buffer size.
+  size_t Size() const                                     { return size_; }
+
+  /// Data returns a pointer to the data buffer.
+  pointer Data()                                          { return &data_; }
+  const_pointer Data() const                              { return data_; }
+
+ private:
+  std::array<size_t, _Rank>   dimensions_;
+  std::array<ssize_t, _Rank>  strides_;
+  size_t                      size_;
+  pointer                     data_;
+};
+
+} // end of namespace grid
+
+#endif  // GRID_TENSOR_BASE_TENSOR_VIEW_H
+
