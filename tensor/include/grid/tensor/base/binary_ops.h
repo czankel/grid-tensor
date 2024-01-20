2636a98cbd1c047390ce06dd6b7684947a305542
Chris Zankel <chris@zankel.net>
2024-01-20 12:46:09 -0800

    tensor/base: refactor BinaryOps/BinaryOperator define Add/Sub/Mul/Div
    Refactor BinaryOps to make use of the changes to better align to ranges.
    
    Define lazy binary functions for element-wise adding, subtracting,
    multiplying, and dividing two tensors.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/base/binary_ops.h
+++ tensor/include/grid/tensor/base/binary_ops.h
@@ -12,69 +12,37 @@
 #define GRID_TENSOR_BINARY_OP_H
 
 #include <span>
+#include <algorithm>
+#include <ranges>
 
 #include "../concepts.h"
+#include "../binary_function.h"
 
 namespace grid {
 namespace tensor {
 
-/// BinaryOp<Tensor> implements element-wise binary operations of two tensors.
+/// BinaryOperator<Operator> implements element-wise binary operations of two tensors.
 /// The dimensions of the tensors must match following broadcasting rules.
 /// The resulting rank is the maximum of the tensor ranks.
 ///
-///  @tparm T         resulting value type
-///  @tparm TOp       binary operation
-///  @tparm TTensor1  LHS tensor
-///  @tparm TTensor2  RHS tensor
-///
-/// Broadcasting rules:
-///
-///  - Lower ranking tensors are extended and filled with dimension 1 and stride 0 on the left.
-///  - Staring from the right, the dimensions must be either identical or 1.
-///
-/// Examples:
-///   shape: 3, 4, 4 <op> shape: 1, 4, 1    -> OK
-///   shape:    4, 1 <op> shape: 3, 4, 3    -> OK
-///   shape: 3, 4, 4 <op> shape: 3, 5, 1    -> Error
-///
-template <typename T, typename TOp, PrimitiveTensor TTensor1, PrimitiveTensor TTensor2>
-class BinaryOp
+///  @tparm TOperator binary operator
+template <typename TOperator>
+class BinaryOperator
 {
- public:
-  using value_type = T;
-  using pointer = T*;
-  using const_pointer = const T*;
-  using tensor1_type = std::remove_reference_t<TTensor1>;
-  using tensor2_type = std::remove_reference_t<TTensor2>;
-  constexpr static size_t rank = std::max(tensor1_type::rank, tensor2_type::rank);
-
-  template <typename T1, typename T2>
-  BinaryOp(T1&& tensor1, T2&& tensor2)
-   : tensor1_(std::forward<T1>(tensor1)),
-     tensor2_(std::forward<T2>(tensor2))
-  {}
-
-  ~BinaryOp() {}
-
-  // delete assignment and copy/move constructors
-  BinaryOp() = delete;
-  BinaryOp(const BinaryOp& other) = delete;
-  BinaryOp(BinaryOp&& other) = delete;
-  BinaryOp& operator=(const BinaryOp& other) = delete;
-  BinaryOp& operator=(BinaryOp&& other) = delete;
-
  private:
   // operation on a single element
+  template <typename const_pointer, typename pointer>
   inline void eval(pointer dest, const_pointer src1, const_pointer src2,
                   std::span<const size_t,  0> dimensions,
                   std::span<const ssize_t, 0>,
                   std::span<const ssize_t, 0>,
                   std::span<const ssize_t, 0>) const
   {
-    TOp::eval(dest, src1, src2);
+    TOperator::eval(dest, src1, src2);
   }
 
   // operation on a single dimension (unoptimized)
+  template <typename const_pointer, typename pointer>
   inline void eval(pointer dest, const_pointer src1, const_pointer src2,
                   std::span<const size_t,  1> dimensions,
                   std::span<const ssize_t, 1>,
@@ -83,14 +51,15 @@ class BinaryOp
   {
     for (size_t i = 0; i < dimensions[0]; i++)
     {
-      TOp::eval(dest + i, src1, src2);
+      TOperator::eval(dest + i, src1, src2);
       reinterpret_cast<const char*&>(src1) += strides1[0];
       reinterpret_cast<const char*&>(src2) += strides2[0];
     }
   }
 
   // operation on higher dimensions (unoptimized)
-  template <size_t N> inline
+  template <size_t N, typename const_pointer, typename pointer> inline
+  //template <size_t N> inline
   void eval(pointer dest, const_pointer src1, const_pointer src2,
            std::span<const size_t,  N> dimensions,
            std::span<const ssize_t, N> strides0,
@@ -111,62 +80,86 @@ class BinaryOp
       reinterpret_cast<const char*&>(src2) += strides2[0];
     }
   }
+
  public:
-
-  /// operator()() evaluates the binary operation and returns a tensor.
-  auto operator()() const
+  template <typename T, size_t TRank>
+  void operator()(T* dst, const T* src1, const T* src2,
+                  const std::array<size_t, TRank>& dimensions,
+                  const std::array<ssize_t, TRank>& strides0,
+                  const std::array<ssize_t, TRank>& strides1,
+                  const std::array<ssize_t, TRank>& strides2)
   {
-    auto [dimensions, strides1, strides2] = Broadcast(tensor1_, tensor2_);
-    auto result = Tensor(dimensions, Uninitialized<value_type>{});
-
-    eval(result.Data(),
-        tensor1_.Data(),
-        tensor2_.Data(),
-        std::span<const size_t, rank>(dimensions),
-        std::span<const ssize_t, rank>(result.Strides()),
-        std::span<const ssize_t, rank>(strides1),
-        std::span<const ssize_t, rank>(strides2));
-    return result;
+    eval(dst, src1, src2,
+      std::span<const size_t, TRank>(dimensions),
+      std::span<const ssize_t, TRank>(strides0),
+      std::span<const ssize_t, TRank>(strides1),
+      std::span<const ssize_t, TRank>(strides2));
   }
-
- private:
-  TTensor1 tensor1_;
-  TTensor2 tensor2_;
 };
 
 //
 // Operators
 //
 
-template<typename T> struct AddOp
+struct AddOperator
 {
   // scalar X scalar
+  template<typename T>
   static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 + *src2; }
 };
 
-/// Add adds two tensors element-wise.
-template <PrimitiveTensor T1, PrimitiveTensor T2>
-auto Add(T1&& tensor1, T2&& tensor2)
-{
-  using T = typename std::decay_t<T1>::value_type;
-  return BinaryOp<T, AddOp<T>, typename to_tensor<T1>::type, typename to_tensor<T2>::type>(
-      std::forward<T1>(tensor1), std::forward<T2>(tensor2));
-};
-
-template<typename T> struct MulOp
+struct SubOperator
 {
   // scalar X scalar
+  template<typename T>
+  static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 - *src2; }
+};
+
+struct MulOperator
+{
+  // scalar X scalar
+  template<typename T>
   static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 * *src2; }
 };
 
-/// Mul multiplies two tensors element-wise.
-template <PrimitiveTensor T1, PrimitiveTensor T2>
-auto Mul(T1&& tensor1, T2&& tensor2)
+struct DivOperator
 {
-  using T = typename std::decay_t<T1>::value_type;
-  return BinaryOp<T, MulOp<T>, typename to_tensor<T1>::type, typename to_tensor<T2>::type>(
-      std::forward<T1>(tensor1), std::forward<T2>(tensor2));
+  // scalar X scalar
+  template<typename T>
+  static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 / *src2; }
 };
+
+//
+// Exported binary functions
+//
+
+/// @brief Add adds two tensors element-wise (lazily).
+template <PrimitiveTensor TTensor1, PrimitiveTensor TTensor2>
+auto Add(TTensor1&& tensor1, TTensor2&& tensor2)
+{
+  return BinaryFunction(BinaryOperator<AddOperator>{}, std::forward<TTensor1>(tensor1), std::forward<TTensor2>(tensor2));
+}
+
+/// @brief Sub subtracts two tensors element-wise (lazily).
+template <PrimitiveTensor TTensor1, PrimitiveTensor TTensor2>
+auto Sub(TTensor1&& tensor1, TTensor2&& tensor2)
+{
+  return BinaryFunction(SubOperator{}, std::forward<TTensor1>(tensor1), std::forward<TTensor2>(tensor2));
+}
+
+/// @brief Mul multiplies two tensors element-wise (lazily).
+template <PrimitiveTensor TTensor1, PrimitiveTensor TTensor2>
+auto Mul(TTensor1&& tensor1, TTensor2&& tensor2)
+{
+  return BinaryFunction(MulOperator{}, std::forward<TTensor1>(tensor1), std::forward<TTensor2>(tensor2));
+}
+
+/// @brief Div multiplies two tensors element-wise (lazily).
+template <PrimitiveTensor TTensor1, PrimitiveTensor TTensor2>
+auto Div(TTensor1&& tensor1, TTensor2&& tensor2)
+{
+  return BinaryFunction(DivOperator{}, std::forward<TTensor1>(tensor1), std::forward<TTensor2>(tensor2));
+}
 
 
 } // end of namespace tensor
