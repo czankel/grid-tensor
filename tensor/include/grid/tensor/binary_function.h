fa2c3c6529fe6cf6d85a7523b51656ad4a297dd6
Chris Zankel <chris@zankel.net>
2024-01-20 12:42:48 -0800

    tensor: implmenet BinaryFunction for a lazy element-wise binary operation
    The tensor-pair is stored and only evaluated when required.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

---
+++ tensor/include/grid/tensor/binary_function.h
@@ -0,0 +1,95 @@
+//
+// Copyright (C) Chris Zankel. All rights reserved.
+// This code is subject to U.S. and other copyright laws and
+// intellectual property protections.
+//
+// The contents of this file are confidential and proprietary to Chris Zankel.
+//
+
+#ifndef GRID_TENSOR_BINARY_FUNCTION_H
+#define GRID_TENSOR_BINARY_FUNCTION_H
+
+#include <span>
+#include <algorithm>
+#include <ranges>
+
+#include "concepts.h"
+#include "transform.h"
+
+namespace grid {
+namespace tensor {
+
+
+/// @brief BinaryFunction<Operator> implements lazy element-wise binary operations of two tensors.
+///
+/// BinaryFunction stores the tensors (or tensor operators) and evaluates it on execution with
+/// the operator().
+///
+/// The dimensions of the tensors must match following broadcasting rules.
+/// The resulting rank is the maximum of the tensor ranks.
+///
+///  @tparm TOperator binary operator
+///  @tparm TTensor1  first tensor
+///  @tparm TTensor2  second tensor
+///
+/// Broadcasting rules:
+///
+///  - Lower ranking tensors are extended and filled with dimension 1 and stride 0 on the left.
+///  - Staring from the right, the dimensions must be either identical or 1.
+///
+/// Examples:
+///   shape: 3, 4, 4 <op> shape: 1, 4, 1    -> OK
+///   shape:    4, 1 <op> shape: 3, 4, 3    -> OK
+///   shape: 3, 4, 4 <op> shape: 3, 5, 1    -> Error
+///
+template <typename TOperator, AnyTensor TTensor1, AnyTensor TTensor2>
+class BinaryFunction
+{
+ public:
+  using tensor1_type = std::remove_reference_t<TTensor1>;
+  using tensor2_type = std::remove_reference_t<TTensor2>;
+  using value_type = std::common_type_t<typename tensor1_type::value_type, typename tensor2_type::value_type>;
+  using pointer = value_type*;
+  using const_pointer = const value_type*;
+  constexpr static size_t rank = std::max(tensor1_type::rank, tensor2_type::rank);
+
+  template <typename T1, typename T2>
+  BinaryFunction(TOperator, T1&& tensor1, T2&& tensor2)
+   : tensor1_(std::forward<T1>(tensor1)),
+     tensor2_(std::forward<T2>(tensor2))
+  {}
+
+  ~BinaryFunction() {}
+
+  // delete assignment and copy/move constructors
+  BinaryFunction() = delete;
+  BinaryFunction(const BinaryFunction& other) = delete;
+  BinaryFunction(BinaryFunction&& other) = delete;
+  BinaryFunction& operator=(const BinaryFunction& other) = delete;
+  BinaryFunction& operator=(BinaryFunction&& other) = delete;
+
+ public:
+
+  /// operator()() evaluates the binary operator and returns a tensor.
+  auto operator()() const
+  {
+    auto [dimensions, strides1, strides2] = Broadcast(tensor1_, tensor2_);
+    auto result = Tensor(dimensions, Uninitialized<value_type>{});
+    Transform(TensorView(tensor1_, dimensions, strides1, tensor1_.Size()),
+              TensorView(tensor2_, dimensions, strides2, tensor2_.Size()),
+              result.begin(), TOperator{});
+    return result;
+  }
+
+ private:
+  TTensor1 tensor1_;
+  TTensor2 tensor2_;
+};
+
+template <typename TOp, typename T1, typename T2> BinaryFunction(TOp, T1&&, T2&&) -> BinaryFunction<TOp, T1&&, T2&&>;
+
+
+} // end of namespace tensor
+} // end of namespace grd
+
+#endif  // GRID_TENSOR_BINARY_FUNCTION_H
