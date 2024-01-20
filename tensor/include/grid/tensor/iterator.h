d81fd5574b129b1f6bbfc19843e126c000ae076e
Chris Zankel <chris@zankel.net>
2024-01-20 12:28:17 -0800

    tensor: add constant interator: ConstIterator
    It's currently a duplication of Iterator but using a const value type.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/iterator.h
+++ tensor/include/grid/tensor/iterator.h
@@ -16,6 +16,8 @@ namespace details {
 
 class Tensor;
 
+// TODO: ConstIterator is mostly a duplicate for Iterator but for const_pointer
+
 /// Iterator implements a bidirection iterator for tensors.
 template <typename TTensor>
 class Iterator
@@ -115,6 +117,113 @@ class Iterator
   pointer                   data_;
   const TTensor*            tensor_;
 };
+
+
+/// ConstIterator implements a bidirection iterator for tensors.
+template <typename TTensor>
+class ConstIterator
+{
+ public:
+  using iterator_category = std::bidirectional_iterator_tag;
+  using difference_type   = std::ptrdiff_t;
+  using value_type        = typename TTensor::value_type;
+  using const_pointer     = typename TTensor::const_pointer;
+  using const_reference   = typename TTensor::const_reference;
+  static constexpr size_t rank = TTensor::rank;
+
+  ConstIterator() = default;
+
+  // Note that ranges iterators must be default-initializable, so cannot use reference for Tensor
+  ConstIterator(const TTensor* tensor, const_pointer data)
+    : coordinates_{0},
+      ptr_(data),
+      data_(data),
+      tensor_(tensor)
+  {}
+
+  // FIXME: dimensions are a position and the position must be added to ptr_
+  ConstIterator(const TTensor* tensor, const_pointer data, const std::array<size_t, rank>& dimensions)
+    : coordinates_(dimensions),
+      ptr_(data),
+      data_(data),
+      tensor_(tensor)
+  {}
+
+  const_reference operator*() const                { return *ptr_; }
+
+  // TODO: unrol loops?
+  ConstIterator& operator++()
+  {
+    auto& extents = tensor_->Dimensions();
+    auto& strides = tensor_->Strides();
+
+    ptr_++;
+    size_t index = 0;
+    for (; index < rank && ++coordinates_[index] == extents[index]; index++)
+      coordinates_[index] = 0;
+    // non-contiguous, note that all coordinate's so far are 0
+    if (index > 0)
+      for (ptr_ = data_; index < rank; index++)
+        reinterpret_cast<char*&>(ptr_) += coordinates_[index] * strides[index];
+
+    return *this;
+  }
+
+  ConstIterator& operator--()
+  {
+    auto& extents = tensor_->Dimensions();
+    auto& strides = tensor_->Strides();
+
+    // TODO: assert > data_
+    ptr_--;
+    size_t index = 0;
+    for (; index < rank && --coordinates_[index] == 0; index++)
+      coordinates_[index] = extents[index];
+    // non-contiguous, note that all coordinate's so far are 0
+    if (index > 0)
+      for (ptr_ = data_; index < rank; index++)
+        reinterpret_cast<char*&>(ptr_) += coordinates_[index] * strides[index];
+
+    return *this;
+  }
+
+  ConstIterator operator++(int)                  { ConstIterator tmp = *this; ++(*this); return tmp; }
+  ConstIterator operator--(int)                  { ConstIterator tmp = *this; --(*this); return tmp; }
+
+  /// @brief return a new iterator with added distances to the positions for the provided dimensions.
+  template <size_t S>
+  ConstIterator operator+(const std::array<size_t, S>& dist)
+  {
+    ConstIterator tmp = *this;
+    for (size_t i = 0; i < S; i++)
+      tmp.coordinates_[i] += dist[i];
+    return tmp;
+  }
+
+  /// @brief add distances to the positions of the iterator for the provided dimensions.
+  template <size_t S>
+  ConstIterator& operator+=(const std::array<size_t, S>& dist)
+  {
+    for (size_t i = 0; i < S; i++)
+      coordinates_[i] += dist[i];
+    return *this;
+  }
+
+  friend bool operator==(const ConstIterator& a, const ConstIterator& b) { return a.ptr_ == b.ptr_; }
+
+  // iterator extensions
+  size_t                            Rank()              { return rank; }
+  const std::array<size_t, rank>&   Extents() const     { return tensor_->Dimensions(); }
+  const std::array<ssize_t, rank>&  Strides() const     { return tensor_->Strides(); }
+  const std::array<size_t, rank>&   Coordinates() const { return coordinates_; }
+
+ private:
+  std::array<size_t, rank>  coordinates_;
+  const_pointer             ptr_;
+  const_pointer             data_;
+  const TTensor*            tensor_;
+};
+
 
 } // end of namespace details
 } // end of namespace grid
