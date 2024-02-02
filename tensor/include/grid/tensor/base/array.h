9e8d6104ef9816f22341c51ecb58105a9b98440d
Chris Zankel <chris@zankel.net>
2024-02-01 20:25:29 -0800

    tensor/base: move Array into it's own file
    No other functional changes
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

---
+++ tensor/include/grid/tensor/base/array.h
@@ -0,0 +1,91 @@
+//
+// Copyright (C) Chris Zankel. All rights reserved.
+// This code is subject to U.S. and other copyright laws and
+// intellectual property protections.
+//
+// The contents of this file are confidential and proprietary to Chris Zankel.
+//
+
+// DO NOT INCLUDE THIS FILE DIRECTLY
+
+#ifndef GRID_TENSOR_BASE_ARRAY_H
+#define GRID_TENSOR_BASE_ARRAY_H
+
+namespace grid {
+
+/// brief: Array is a dynamically allocated buffer.
+template <typename T>
+class Array
+{
+  using value_type = T;
+  using pointer = value_type*;
+  using const_pointer = const value_type*;
+
+ public:
+  Array() = default;
+
+  // Explicity disallow copy construction as Array isn't fully aware of any buffer structure.
+  Array(const Array& other) = delete;
+
+  // @brief Move constructor.
+  Array(Array&& other) : size_(other.size_), data_(std::move(other.data_)) { other.data_ = nullptr; }
+
+  // @brief Allocates a buffer of the provided size.
+  Array(size_t size)
+    : size_(size),
+      data_(static_cast<pointer>(operator new[](size_, std::align_val_t(16))))
+  {}
+
+  ~Array()
+  {
+    if (data_ != nullptr)
+      operator delete[](data_, std::align_val_t(16));
+  }
+
+  Array& operator=(Array&& other)
+  {
+    if (data_ != nullptr)
+      operator delete[](data_, std::align_val_t(16));
+
+    size_ = other.size_;
+    data_ = std::move(other.data_);
+    other.data_ = nullptr;
+
+    return *this;
+  }
+
+  Array& operator=(const Array& other) = delete;
+
+
+  /// Resize resizes the buffer of the Array. This will destroy
+  Array& Realloc(size_t size)
+  {
+    if (size != size_)
+    {
+      if (data_ != nullptr)
+        operator delete[](data_, std::align_val_t(16));
+      data_ = static_cast<pointer>(operator new[](size_, std::align_val_t(16)));
+    }
+
+    return *this;
+  }
+
+
+  /// Size returns the size of the entire buffer.
+  size_t Size() const                                     { return size_; }
+
+  /// Data returns a pointer to the data buffer.
+  pointer Data()                                          { return data_; }
+
+  /// Data returns a pointer to the data buffer.
+  const_pointer Data() const                              { return data_; }
+
+ protected:
+  size_t  size_;
+  pointer data_;
+};
+
+
+} // end of namespace grid
+
+#endif  // GRID_TENSOR_BASE_ARRAY_H
--- tensor/include/grid/tensor/base/tensor.h
+++ tensor/include/grid/tensor/base/tensor.h
@@ -17,10 +17,10 @@
 #include <numeric>
 #include <tuple>
 
-#include "../array.h"
 #include "../device.h"
 #include "../tensor_parameters.h"
 
+#include "array.h"
 #include "copy.h"
 
 namespace grid {
@@ -46,77 +46,6 @@ inline void initialize(T* dst, std::span<const size_t, N> dimensions, std::span<
 }
 
 } // end of namespace details
-
-template <typename T>
-class Array
-{
-  using value_type = T;
-  using pointer = value_type*;
-  using const_pointer = const value_type*;
-
- public:
-  Array() = default;
-
-  // Explicity disallow copy construction as Array isn't fully aware of any buffer structure.
-  Array(const Array& other) = delete;
-
-  // @brief Move constructor.
-  Array(Array&& other) : size_(other.size_), data_(std::move(other.data_)) { other.data_ = nullptr; }
-
-  // @brief Allocates a buffer of the provided size.
-  Array(size_t size)
-    : size_(size),
-      data_(static_cast<pointer>(operator new[](size_, std::align_val_t(16))))
-  {}
-
-  ~Array()
-  {
-    if (data_ != nullptr)
-      operator delete[](data_, std::align_val_t(16));
-  }
-
-  Array& operator=(Array&& other)
-  {
-    if (data_ != nullptr)
-      operator delete[](data_, std::align_val_t(16));
-
-    size_ = other.size_;
-    data_ = std::move(other.data_);
-    other.data_ = nullptr;
-
-    return *this;
-  }
-
-  Array& operator=(const Array& other) = delete;
-
-
-  /// Resize resizes the buffer of the Array. This will destroy
-  Array& Realloc(size_t size)
-  {
-    if (size != size_)
-    {
-      if (data_ != nullptr)
-        operator delete[](data_, std::align_val_t(16));
-      data_ = static_cast<pointer>(operator new[](size_, std::align_val_t(16)));
-    }
-
-    return *this;
-  }
-
-
-  /// Size returns the size of the entire buffer.
-  size_t Size() const                                     { return size_; }
-
-  /// Data returns a pointer to the data buffer.
-  pointer Data()                                          { return data_; }
-
-  /// Data returns a pointer to the data buffer.
-  const_pointer Data() const                              { return data_; }
-
- protected:
-  size_t  size_;
-  pointer data_;
-};
 
 
 // FIXME: the coordinates of the first tensor must be added to the
