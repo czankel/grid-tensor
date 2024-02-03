b99465f12cbc7883fa12ee041734aac6c20e0ef1
Chris Zankel <chris@zankel.net>
2024-02-03 11:30:09 -0800

    tensor/base: implement binary ops for contiguous data
    Split implementation into supporting contiguous and non-contiguous
    versions. A mix of contiguous/non-contiguous sources and destinations is
    not supported, also needs analysis and more optimizations
    (vectorization/parallelization).
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/base/binary_ops.h
+++ tensor/include/grid/tensor/base/binary_ops.h
@@ -28,6 +28,7 @@ template <typename TOperator>
 class BinaryOperator
 {
  private:
+
   // operation on a single element
   template <typename const_pointer, typename pointer>
   inline void eval(pointer dest, const_pointer src1, const_pointer src2,
@@ -36,28 +37,27 @@ class BinaryOperator
                   std::span<const ssize_t, 0>,
                   std::span<const ssize_t, 0>) const
   {
-    TOperator::eval(dest, src1, src2);
+    TOperator::eval(dest, src1, src2, 0);
   }
 
   // operation on a single dimension (unoptimized)
   template <typename const_pointer, typename pointer>
   inline void eval(pointer dest, const_pointer src1, const_pointer src2,
                   std::span<const size_t,  1> dimensions,
-                  std::span<const ssize_t, 1>,
+                  std::span<const ssize_t, 1> strides0,
                   std::span<const ssize_t, 1> strides1,
                   std::span<const ssize_t, 1> strides2) const
   {
     for (size_t i = 0; i < dimensions[0]; i++)
     {
-      TOperator::eval(dest + i, src1, src2);
+      TOperator::eval(dest, src1, src2, 0);
+      dest += strides0[0];
       src1 += strides1[0];
       src2 += strides2[0];
     }
   }
 
-  // operation on higher dimensions (unoptimized)
   template <size_t N, typename const_pointer, typename pointer> inline
-  //template <size_t N> inline
   void eval(pointer dest, const_pointer src1, const_pointer src2,
            std::span<const size_t,  N> dimensions,
            std::span<const ssize_t, N> strides0,
@@ -68,14 +68,69 @@ class BinaryOperator
     for (size_t i = 0; i < dimensions[0]; i++)
     {
       eval(dest, src1, src2,
-          std::span<const size_t,  N - 1>(dimensions.begin() + 1, N - 1),
-          std::span<const ssize_t, N - 1>(strides0.begin() + 1, N - 1),
-          std::span<const ssize_t, N - 1>(strides1.begin() + 1, N - 1),
-          std::span<const ssize_t, N - 1>(strides2.begin() + 1, N - 1));
-
+           std::span<const size_t,  N - 1>(dimensions.begin() + 1, N - 1),
+           std::span<const ssize_t, N - 1>(strides0.begin() + 1, N - 1),
+           std::span<const ssize_t, N - 1>(strides1.begin() + 1, N - 1),
+           std::span<const ssize_t, N - 1>(strides2.begin() + 1, N - 1));
       dest += strides0[0];
       src1 += strides1[0];
       src2 += strides2[0];
+    }
+  }
+
+  //
+  // optimized versions for contiguous tensors
+  //
+
+  template <size_t N, typename const_pointer, typename pointer>
+  inline void eval(pointer dest, const_pointer src1, const_pointer src2,
+                   size_t dimensions0,
+                   std::span<const size_t,  0>,
+                   std::span<const ssize_t, 0>,
+                   std::span<const ssize_t, 0>,
+                   std::span<const ssize_t, 0>)
+  {
+    for (size_t i = 0; i < dimensions0; i++)
+      TOperator::eval(dest, src1, src2, i);
+  }
+
+
+  // optimized for contiguous tensors
+  template <size_t N, typename const_pointer, typename pointer>
+  inline void eval(pointer dest, const_pointer src1, const_pointer src2,
+                   size_t dimensions0,
+                   std::span<const size_t,  N> dimensions,
+                   std::span<const ssize_t, N> strides0,
+                   std::span<const ssize_t, N> strides1,
+                   std::span<const ssize_t, N> strides2) const
+  {
+    static_assert(N != std::dynamic_extent, "dynamic_extent not allowed");
+    // "fold" one rank
+    if (strides0[N - 2] - dimensions[N - 1] == 0 &&
+        strides1[N - 2] - dimensions[N - 1] == 0 &&
+        strides2[N - 2] - dimensions[N - 1] == 0)
+    {
+      eval(dest, src1, src2,
+           dimensions[N - 2] * dimensions[N - 1],
+           std::span<const size_t,  N - 1>(dimensions.begin(), N - 1),
+           std::span<const ssize_t, N - 1>(strides0.begin(), N - 1),
+           std::span<const ssize_t, N - 1>(strides1.begin(), N - 1),
+           std::span<const ssize_t, N - 1>(strides2.begin(), N - 1));
+    }
+    else
+    {
+      for (size_t i = 0; i < dimensions0; i++)
+      {
+        eval(dest, src1, src2,
+             dimensions[N - 1],
+             std::span<const size_t,  N - 1>(dimensions.begin() + 1, N - 1),
+             std::span<const ssize_t, N - 1>(strides0.begin() + 1, N - 1),
+             std::span<const ssize_t, N - 1>(strides1.begin() + 1, N - 1),
+             std::span<const ssize_t, N - 1>(strides2.begin() + 1, N - 1));
+        dest += strides0[0];
+        src1 += strides1[0];
+        src2 += strides2[0];
+      }
     }
   }
 
@@ -87,11 +142,26 @@ class BinaryOperator
                   const std::array<ssize_t, TRank>& strides1,
                   const std::array<ssize_t, TRank>& strides2)
   {
+    if constexpr (TRank > 2)
+    {
+      if (strides0[TRank - 2] - dimensions[TRank - 1] == 0 &&
+          strides1[TRank - 2] - dimensions[TRank - 1] == 0 &&
+          strides2[TRank - 2] - dimensions[TRank - 1] == 0)
+        eval(dst, src1, src2,
+             dimensions[TRank - 1],
+             std::span<const size_t,  TRank - 1>(dimensions.begin(), TRank - 1),
+             std::span<const ssize_t, TRank - 1>(strides0.begin(), TRank - 1),
+             std::span<const ssize_t, TRank - 1>(strides1.begin(), TRank - 1),
+             std::span<const ssize_t, TRank - 1>(strides2.begin(), TRank - 1));
+      return;
+    }
+
+
     eval(dst, src1, src2,
-      std::span<const size_t, TRank>(dimensions),
-      std::span<const ssize_t, TRank>(strides0),
-      std::span<const ssize_t, TRank>(strides1),
-      std::span<const ssize_t, TRank>(strides2));
+         std::span<const size_t,  TRank>(dimensions),
+         std::span<const ssize_t, TRank>(strides0),
+         std::span<const ssize_t, TRank>(strides1),
+         std::span<const ssize_t, TRank>(strides2));
   }
 };
 
@@ -103,28 +173,28 @@ struct AddOperator
 {
   // scalar X scalar
   template<typename T>
-  static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 + *src2; }
+  static inline void eval(T* dest, const T* src1, const T* src2, const size_t i) { dest[i] = src1[i] + src2[i]; }
 };
 
 struct SubOperator
 {
   // scalar X scalar
   template<typename T>
-  static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 - *src2; }
+  static inline void eval(T* dest, const T* src1, const T* src2, const size_t i) { dest[i] = src1[i] - src2[i]; }
 };
 
 struct MulOperator
 {
   // scalar X scalar
   template<typename T>
-  static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 * *src2; }
+  static inline void eval(T* dest, const T* src1, const T* src2, const size_t i) { dest[i] = src1[i] * src2[i]; }
 };
 
 struct DivOperator
 {
   // scalar X scalar
   template<typename T>
-  static inline void eval(T* dest, const T* src1, const T* src2) { *dest = *src1 / *src2; }
+  static inline void eval(T* dest, const T* src1, const T* src2, const size_t i) { dest[i] = src1[i] / src2[i]; }
 };
 
 
