b32e300c0336555d28b50bdb75f244db6b9d5351
Chris Zankel <chris@zankel.net>
2024-02-11 08:26:39 -0800

    tensor/base: improve matmul for contiguous tensors
    Using simpler for-loops (i++) enables the compiler to do more
    optimizations (e.g. vectorization).
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- tensor/include/grid/tensor/base/matmul.h
+++ tensor/include/grid/tensor/base/matmul.h
@@ -19,20 +19,6 @@ namespace grid {
 class MatMulOperator
 {
  private:
-  template <typename T>
-  inline void VecDot(T* dest, const T* src1, const T* src2,
-                     size_t dimensions, ssize_t strides1, ssize_t strides2) const
-  {
-    T sum{0};
-    for (size_t i = 0; i < dimensions; i++)
-    {
-      sum += *src1 * *src2;
-      src1 += strides1;
-      src2 += strides2;
-    }
-    *dest = sum;
-  }
-
   // Note that dimensions are mkn: M_m_k * M_k_n -> M(m,n)
   // Note that strides for all tensors (destination and sources) are:
   //    [0] row: m -> m + 1,  [1] col: n -> n + 1
@@ -43,18 +29,71 @@ class MatMulOperator
                      std::span<const ssize_t, 2> strides1,
                      std::span<const ssize_t, 2> strides2) const
   {
-    for (size_t m = 0; m < dimensions[0]; m++)
+    // optimizations:
+    // vec = mat x vec
+    // mat = mat x vec(broadcast)
+    // mat = vec(broadcast) x mat
+    if (strides0[1] <= 1 && strides1[1] <= 1 && strides2[0] <= 1)
     {
-      T* destprime = dest;
-      const T* src2prime = src2;
-      for (size_t n = 0; n < dimensions[2]; n++)
+      // all tensors are contiguous
+      if (strides0[0] - dimensions[1] == 0 &&
+          strides1[0] - dimensions[1] == 0 &&
+          strides2[1] - dimensions[1] == 0)
       {
-        VecDot(destprime, src1, src2prime, dimensions[1], strides1[1], strides2[0]);
-        destprime += strides0[1];
-        src2prime += strides2[1];
+        for (size_t i = 0; i < dimensions[0] * dimensions[2]; i++)
+        {
+          T sum{0};
+          for (size_t k = 0; k < dimensions[1]; k++)
+            sum += src1[k] * src2[k];
+          dest[i] = sum;
+        }
       }
-      dest += strides0[0];
-      src1 += strides1[0];
+      // inner vector dot
+      else
+      {
+        for (size_t m = 0; m < dimensions[0]; m++)
+        {
+          T* destprime = dest;
+          const T* src2prime = src2;
+          for (size_t n = 0; n < dimensions[2]; n++)
+          {
+            T sum{0};
+            for (size_t k = 0; k < dimensions[1]; k++)
+              sum += src1[k] * src2prime[k];
+            *destprime = sum;
+            destprime += strides0[1];
+            src2prime += strides2[1];
+          }
+          dest += strides0[0];
+          src1 += strides1[0];
+        }
+      }
+    }
+    // no optimizations
+    else
+    {
+      for (size_t m = 0; m < dimensions[0]; m++)
+      {
+        T* destprime = dest;
+        const T* src2prime = src2;
+        for (size_t n = 0; n < dimensions[2]; n++)
+        {
+          const T* src1tmp = src1;
+          const T* src2tmp = src2prime;
+          T sum{0};
+          for (size_t i = 0; i < dimensions[1]; i++)
+          {
+            sum += *src1tmp * *src2tmp;
+            src1tmp += strides1[1];
+            src2tmp += strides2[0];
+          }
+          *destprime = sum;
+          destprime += strides0[1];
+          src2prime += strides2[1];
+        }
+        dest += strides0[0];
+        src1 += strides1[0];
+      }
     }
   }
 
@@ -64,7 +103,22 @@ class MatMulOperator
   void operator()(T* dst, const T* src1, const T* src2,
                   const size_t dimensions, const ssize_t strides1, const ssize_t strides2)
   {
-    VecDot(dst, src1, src2, dimensions, strides1, strides2);
+    T sum{0};
+    if (strides1 == 1 && strides2 == 1)
+    {
+      for (size_t i = 0; i < dimensions; i++)
+        sum += src1[i] * src2[i];
+    }
+    else
+    {
+      for (size_t i = 0; i < dimensions; i++)
+      {
+        sum += *src1 * *src2;
+        src1 += strides1;
+        src2 += strides2;
+      }
+    }
+    *dst = sum;
   }
 
   // matmul
