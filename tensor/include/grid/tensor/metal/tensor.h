bcbea40b00c8305bedc337c2bb79bf8db818dec7
Chris Zankel <chris@zankel.net>
2024-05-17 21:53:42 -0700

    tensor/metal: add initial support for Apple Metal [2/4]
    Add header and source files for supporting metal tensors.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

---
+++ tensor/include/grid/tensor/metal/tensor.h
@@ -0,0 +1,16 @@
+//
+// Copyright (C) Chris Zankel. All rights reserved.
+// This code is subject to U.S. and other copyright laws and
+// intellectual property protections.
+//
+// The contents of this file are confidential and proprietary to Chris Zankel.
+//
+
+// DO NOT INCLUDE THIS FILE DIRECTLY
+
+#ifndef GRID_TENSOR_METAL_TENSOR_H
+#define GRID_TENSOR_METAL_TENSOR_H
+
+#include "array.h"
+
+#endif  // GRID_TENSOR_METAL_TENSOR_H
