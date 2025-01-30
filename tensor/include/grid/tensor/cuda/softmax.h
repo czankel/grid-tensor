9daf1410b89a61533aa33651b1b9f6e0be5fe90c
Chris Zankel <chris@zankel.net>
2025-01-29 22:44:49 -0800

    tensor/cuda: add implementation for softmax
    Enable CUDA in softmax unittest and set a more lenient margin for the
    precision. The comparision values in the unit test were created from
    some PC and are slightly different for CUDA.
    
    Some rearrangement of the unittest code.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

---
+++ tensor/include/grid/tensor/cuda/softmax.h
@@ -0,0 +1,77 @@
+//
+// Copyright (C) Chris Zankel. All rights reserved.
+// This code is subject to U.S. and other copyright laws and
+// intellectual property protections.
+//
+// The contents of this file are confidential and proprietary to Chris Zankel.
+//
+
+// DO NOT INCLUDE THIS FILE DIRECTLY
+
+#ifndef GRID_TENSOR_CUDA_SOFTMAX_H
+#define GRID_TENSOR_CUDA_SOFTMAX_H
+
+#include <span>
+#include <algorithm>
+#include <ranges>
+
+#include "../precision.h"
+
+namespace grid {
+
+void CudaDeviceSynchronize(); // FIXME move to some header?
+
+// FIXME: this is just boilerplate code??
+
+/// SoftMaxOperator implements the softmax operation
+///
+///  @tparm TOperator binary operator
+template <> class SoftMaxOperator<device::Cuda>
+{
+  template <typename T, size_t R>
+  void EvalContiguous(T*, const T*,
+                      std::span<const size_t, R>,
+                      std::span<const ssize_t, R>,
+                      std::span<const ssize_t, R>) const;
+
+  template <typename T, size_t R>
+  void EvalDiscontiguous(T*, const T*,
+                         std::span<const size_t, R>,
+                         std::span<const ssize_t, R>,
+                         std::span<const ssize_t, R>) const;
+
+ public:
+
+#if !defined(__CUDACC__)
+
+  template<std::ranges::input_range I,
+           std::ranges::output_range<std::iter_value_t<std::ranges::iterator_t<I>>> O>
+  requires std::indirectly_copyable<std::ranges::iterator_t<I>, std::ranges::iterator_t<O>>
+  void operator()(I&& in, O&& out) const
+  {
+    auto first_d = std::ranges::begin(out);
+    auto first_x = std::ranges::cbegin(in);
+
+    FoldBroadcast([&](auto dimensions, auto strides_d, auto strides_x) {
+
+        constexpr size_t rank = dimensions.size();
+
+        if constexpr (rank > 3)
+          throw std::runtime_error("non-coontiguous tensors of rank > 3 not supported");
+
+        if (IsContiguous(strides_d, strides_x))
+          EvalContiguous(&*first_d, &*first_x, dimensions, strides_d, strides_x);
+        else
+          EvalDiscontiguous(&*first_d, &*first_x, dimensions, strides_d, strides_x);
+
+        CudaDeviceSynchronize(); // FIXME
+
+    }, std::span(first_d.Extents()), std::span(first_d.Strides()), std::span(first_x.Strides()));
+  }
+
+#endif  // !__CUDACC__
+};
+
+} // end of namespace grid
+
+#endif // GRID_TENSOR_CUDA_SOFTMAX_H
--- tensor/unittest/softmax.cc
+++ tensor/unittest/softmax.cc
@@ -7,6 +7,7 @@
 //
 
 #include <grid/tensor/tensor.h>
+#include <grid/tensor/precision.h>
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
@@ -21,12 +22,14 @@
 #include "tensor_metal.h"
 #endif
 
+#ifdef BUILD_CUDA
+#include <grid/tensor/cuda/tensor.h>
+#include <grid/tensor/cuda/softmax.h>
+#include "tensor_cuda.h"
+#endif
+
 
 using testing::ElementsAre;
-
-namespace {
-template <typename _T> constexpr size_t size(size_t count) { return sizeof(_T) * count; }
-}
 
 
 template <typename T> class SoftMaxTestSuite : public testing::Test {};
@@ -35,11 +38,14 @@ TYPED_TEST_SUITE_P(SoftMaxTestSuite);
 
 TYPED_TEST_P(SoftMaxTestSuite, TensorSoftMaxRank1)
 {
-  typename TypeParam::Tensor tensor = grid::Tensor{ 1.618f, 2.f, 3.14f, 5.382f, -8.5f, 1.3f, -2.1f, 3.477f, 5.5f };
-  grid::Tensor expected { 0.944665670912764495e-02f, 0.138413555743161758e-01f, 0.432787127410648800e-01f,
-                          0.407345162648720671e-00f, 0.381141786703997885e-06f, 0.687341376275671639e-02f,
-                          0.229388293053500676e-03f, 0.606221835647282256e-01f, 0.458362745564445395e-00f };
+  typename TypeParam::Tensor tensor =
+    grid::Tensor{ 1.618f, 2.f, 3.14f, 5.382f, -8.5f, 1.3f, -2.1f, 3.477f, 5.5f };
+  grid::Tensor expected {
+    0.944665670912764495e-02f, 0.138413555743161758e-01f, 0.432787127410648800e-01f,
+    0.407345162648720671e-00f, 0.381141786703997885e-06f, 0.687341376275671639e-02f,
+    0.229388293053500676e-03f, 0.606221835647282256e-01f, 0.458362745564445395e-00f };
 
+  grid::Precision p(100.f);
   typename TypeParam::Tensor result = grid::SoftMax(tensor);
   EXPECT_EQ(result, expected);
 }
@@ -48,8 +54,10 @@ TYPED_TEST_P(SoftMaxTestSuite, TensorSoftMaxRank1)
 REGISTER_TYPED_TEST_SUITE_P(SoftMaxTestSuite,
     TensorSoftMaxRank1);
 
-
 INSTANTIATE_TYPED_TEST_SUITE_P(SoftMaxTestBase, SoftMaxTestSuite, TensorBaseType);
 #ifdef BUILD_METAL
 INSTANTIATE_TYPED_TEST_SUITE_P(SoftMaxTestMetal, SoftMaxTestSuite, TensorMetalType);
+#endif
+#ifdef BUILD_CUDA
+INSTANTIATE_TYPED_TEST_SUITE_P(SoftMaxTestCuda, SoftMaxTestSuite, TensorCudaType);
 #endif
